5c3caf45b44d85380695b52537912ffe799896b2
jules <invertime.off@gmail.com>
2021-04-18 19:49:24 +0200

    new landing page design

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,12 +1,21 @@
 import { useEffect, useState, useRef } from "react";
 import { Link, useHistory } from "react-router-dom";
 import localforage from "localforage";
+import Forage from "react-localforage";
 import JSZip from "jszip";
+import "../styles/Welcome.scss";
 
-import pagesStyles from "../styles/Welcome.style";
+//import pagesStyles from "../styles/Welcome.style";
 
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@material-ui/core";
-import { InsertDriveFile, OpenInNew, Add } from '@material-ui/icons';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
+import { InsertDriveFile, OpenInNew, Add } from "@material-ui/icons";
 
 // images
 // import launchpadImage from "../images/launchpad.png"
@@ -37,120 +46,107 @@ export default function Welcome() {
 
     // Load the ZIP file
     JSZip.loadAsync(e.target.files[0]).then((zip) => {
-
       // Grab the /cover.json file and read it...
-      zip.file("cover.json").async("string")
-      .then((data) => {
+      zip
+        .file("cover.json")
+        .async("string")
+        .then((data) => {
+          // Store the content of cover.json to localforage
+          data = JSON.parse(data);
+          localforage.setItem("project", data).then(() => {
+            // Reset `samples` in localforage to store new files
+            localforage.setItem("samples", []).then(() => {
+              // Grab every files of /samples/ folder and add the content to localforage
+              zip.folder("samples").forEach((_, sampleFile) => {
+                sampleFile.async("arraybuffer").then((sampleData) => {
+                  // Take the current samples and push the new sample
+                  localforage.getItem("samples").then((samples) => {
+                    samples.push({
+                      fileName: sampleFile.name,
+                      data: sampleData,
+                    });
 
-        // Store the content of cover.json to localforage
-        data = JSON.parse(data)
-        localforage.setItem("project", data)
-        .then(() => {
+                    // Update the "samples" item
+                    localforage.setItem("samples", samples).then(() => {
+                      console.log(
+                        `[localforage][samples] Added sample content "${sampleFile.name}"`,
+                      );
+                    }); // set the file in localforage
+                  }); // get current samples localforage
+                }); // sample data
+              }); // foreach samples
 
-          // Reset `samples` in localforage to store new files
-          localforage.setItem("samples", [])
-          .then(() => {
+              // Reset file input for new imports...
+              e.target.value = null;
 
-            // Grab every files of /samples/ folder and add the content to localforage
-            zip.folder("samples").forEach((_, sampleFile) => {
-              sampleFile
-              .async("arraybuffer")
-              .then((sampleData) => {
-
-                // Take the current samples and push the new sample
-                localforage.getItem("samples")
-                .then(samples => {
-                  samples.push({
-                    fileName: sampleFile.name,
-                    data: sampleData,
-                  });
-
-                  // Update the "samples" item
-                  localforage.setItem("samples", samples)
-                  .then(() => {
-                    console.log(`[localforage][samples] Added sample content "${sampleFile.name}"`);
-                  }); // set the file in localforage
-                }); // get current samples localforage
-              }); // sample data
-            }); // foreach samples
-
-            // Reset file input for new imports...
-            e.target.value = null;
-
-            // Redirect to /play
-            history.push("/play");
-            console.log("[Welcome][openFileHandler] Done ! Redirected to /play");
-          }); // reset samples localforage
-        }); // store cover.json localforage
-      }); // cover.json load
+              // Redirect to /play
+              history.push("/play");
+              console.log(
+                "[Welcome][openFileHandler] Done ! Redirected to /play",
+              );
+            }); // reset samples localforage
+          }); // store cover.json localforage
+        }); // cover.json load
     }); // ZIP load
   }; // Function end
-
 
   const [warningNewProjectModal, setWarningNewProjectModal] = useState(false);
   const startNewProject = () => {
     if (isAlreadyOpened) {
-      setWarningNewProjectModal(true)
-    }
-    else {
+      setWarningNewProjectModal(true);
+    } else {
       projectEraseAndCreate();
     }
   };
-  
+
   const projectEraseAndCreate = () => {
     const emptyProject = {
-      metas: { title: "Untitled", songAuthor: "Nobody", coverAuthor: "Someone" },
+      metas: {
+        title: "Untitled",
+        songAuthor: "Nobody",
+        coverAuthor: "Someone",
+      },
       samples: [],
-      lights: []
-    }
+      lights: [],
+    };
 
-    localforage.setItem("project", emptyProject)
-    .then(() => {
-      localforage.setItem("samples", [])
-      .then(() => {
+    localforage.setItem("project", emptyProject).then(() => {
+      localforage.setItem("samples", []).then(() => {
         history.push("/play");
-        console.log("[Welcome][startNewProject] localforage has been cleared ! Redirected to /play");
+        console.log(
+          "[Welcome][startNewProject] localforage has been cleared ! Redirected to /play",
+        );
       });
     });
-  }
+  };
+
+  var project;
+
+  localforage.getItem("project").then((data) => {
+    project = data;
+  });
 
   return (
-    <div style={pagesStyles.mainContainer}>
-      <div style={pagesStyles.leftSide}>
-        <h1 style={pagesStyles.title}>
+    <div className="mainContainer">
+      <div className="leftSide">
+        <h1 className="title">
           Welcome to <br /> lpadder !
         </h1>
-        <p style={pagesStyles.subText}>
+        <p className="subText">
           Play Launchpad covers from your browser !{" "}
-          <Link to="/about" style={pagesStyles.link}>
+          <Link to="/about" className="link">
             About this project...
           </Link>
         </p>
       </div>
-      <div style={pagesStyles.rightSide}>
-
-        {isAlreadyOpened && (
-            <Button
-              variant="contained"
-              style={pagesStyles.alreadyOpenedProjectButton}
-              onClick={() => history.push("/play")}
-            >
-              Opened project detected ! Resume it
-              <OpenInNew
-                style={pagesStyles.rightIconButton}
-              />
-            </Button>
-        )}
-        
-        <Button 
+      <div className="rightSide">
+        <Button
           variant="contained"
-          style={pagesStyles.newProjectButton}
+          className="newProjectButton"
           onClick={() => startNewProject()}
         >
           New Project
-          <Add 
-            style={pagesStyles.rightIconButton}
-          />
+          <Add className="rightIconButton" />
         </Button>
 
         <input
@@ -161,29 +157,43 @@ export default function Welcome() {
           style={{ display: "none" }}
         />
 
-        <Button 
+        <Button
           variant="outlined"
-          style={pagesStyles.importProjectButton}
+          className="importProjectButton"
           component="span"
           onClick={() => importFileInput.current.click()}
         >
           Import Project
-          <InsertDriveFile
-            style={pagesStyles.rightIconButton}
-          />
+          <InsertDriveFile className="rightIconButton" />
         </Button>
+
+        {isAlreadyOpened && (
+          <div className="opened">
+            <hr />
+            <p className="openedText">
+              <span className="musicName">test</span>{" "}
+              <span className="authorName">by petit biscuit</span>
+            </p>
+            <Button
+              variant="contained"
+              className="alreadyOpenedProjectButton"
+              onClick={() => history.push("/play")}
+            >
+              Resume it
+              <OpenInNew className="rightIconButton" />
+            </Button>
+          </div>
+        )}
 
         <Dialog
           open={warningNewProjectModal}
           onClose={() => setWarningNewProjectModal(false)}
         >
-          <DialogTitle>
-            {"Start a new project ?"}
-          </DialogTitle>
+          <DialogTitle>{"Start a new project ?"}</DialogTitle>
           <DialogContent>
             <DialogContentText>
-              The currently open project will be erased if you start a new project, and
-              there's no way back !
+              The currently open project will be erased if you start a new
+              project, and there's no way back !
             </DialogContentText>
           </DialogContent>
           <DialogActions>
